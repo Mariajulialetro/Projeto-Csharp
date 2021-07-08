c0e8b060a2c7017f98da3d4750ba8bbf0866f9d7
Mariajulialetro <mariajulialetro@outlook.com>
2021-07-08 18:24:45 -0300

    Projeto c#

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            double salario, vendas, comissão;
+            x = Console.ReadLine();
+
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            comissão = vendas * 0.15 + salario;
+
+            Console.WriteLine("TOTAL = R$ " + comissão.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
